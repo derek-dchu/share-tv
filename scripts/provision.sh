9fa2e5176b9ee6b648f17caa067c8bcc1ab18c54
Derek Hu <derek.dchu@gmail.com>
2015-01-15 03:43:24 -0500

    update provision shell script

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -2,3 +2,9 @@
 
 sudo apt-get -y update
 sudo apt-get -y upgrade
+
+# Install necessary tools
+sudo apt-get install tmux
+
+# Install development tools
+sudo apt-get install nodejs redis-server mongodb
